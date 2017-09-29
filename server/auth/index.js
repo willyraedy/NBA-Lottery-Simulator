82bbb917822e17349e433553008ab15ae0d42157
Willy Raedy <wraedy@gmail.com>
2017-09-29 16:04:02 -0500

    cutout google oauth from boilerplate

--- server/auth/index.js
+++ server/auth/index.js
@@ -36,5 +36,3 @@ router.post('/logout', (req, res) => {
 router.get('/me', (req, res) => {
   res.json(req.user)
 })
-
-router.use('/google', require('./google'))
--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const session = require('express-session')
-const passport = require('passport')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const db = require('./db')
 
@@ -13,23 +12,6 @@ const app = express()
 
 module.exports = app
 
-/**
- * In your development environment, you can keep all of your
- * app's secret API keys in a file called `secrets.js`, in your project
- * root. This file is included in the .gitignore - it will NOT be tracked
- * or show up on Github. On your production server, you can add these
- * keys as environment variables, so that they can still be read by the
- * Node process on process.env
- */
-if (process.env.NODE_ENV !== 'production') require('../secrets');
-
-// passport registration
-passport.serializeUser((user, done) => done(null, user.id))
-passport.deserializeUser((id, done) =>
-  db.models.user.findById(id)
-    .then(user => done(null, user))
-    .catch(done))
-
 const createApp = () => {
   // logging middleware
   app.use(morgan('dev'))
@@ -37,16 +19,6 @@ const createApp = () => {
   // body parsing middleware
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
-
-  // session middleware with passport
-  app.use(session({
-    secret: process.env.SESSION_SECRET || 'my best friend is Cody',
-    store: sessionStore,
-    resave: false,
-    saveUninitialized: false
-  }))
-  app.use(passport.initialize())
-  app.use(passport.session())
 
   // auth and api routes
   app.use('/auth', require('./auth'))
